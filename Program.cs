42b730e44e164774a976a2f68bbd5ee5bcb9a44f
Beratdmrbs <beratdmrbs61@gmail.com>
2024-11-29 20:40:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        List<int> sayilar = new List<int>();
+        sayilar.Add(1);
+        sayilar.Add(2);
+        sayilar.Add(3);
+        sayilar.Add(4);
+        sayilar.Add(5);
+        int toplam = 0;
+        foreach (int sayi in sayilar)
+        {
+            Console.WriteLine(sayi);
+            toplam += sayi;
+        }
+        Console.WriteLine("Toplam:" + toplam);
+    }
+}
